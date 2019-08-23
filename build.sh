6a920899b34438973b8c10773a4e41a1afc61430
liuqi <liuq@simpotech.net>
2019-08-23 10:18:31 +0800

    @base

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ then
     echo '提交信息包含@install,需要更新安装依赖'
     cnpm i
     echo '移动依赖包到统一位置'
-    rm -rf /home/scda/global/node_modules/manager/node_modules
-    mv node_modules /home/scda/global/node_modules/manager/node_modules
+    rm -rf /home/scda/global/node_modules/manager/vue_node_modules
+    mv node_modules /home/scda/global/node_modules/manager/vue_node_modules
 else
     echo '不需要更新依赖'
 fi
 echo '添加依赖软连接到当前路径'
-ln -s /home/scda/global/node_modules/manager/node_modules node_modules
+ln -s /home/scda/global/node_modules/manager/vue_node_modules node_modules
 echo '开始构建项目'
 cnpm run build
 echo '复制构建文件到静态文件目录'
